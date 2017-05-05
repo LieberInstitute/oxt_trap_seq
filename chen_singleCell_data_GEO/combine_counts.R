297e5f27c133d90c4215dbe13ae4a8f4a0c707ac
Andrew Jaffe <andrew.jaffe@libd.org>
2017-05-05 13:50:42 -0400

    uploading single cell data

---
+++ chen_singleCell_data_GEO/combine_counts.R
@@ -0,0 +1,23 @@
+
+library(jaffelab)
+
+# read in data
+fn = list.files("geo_data", full.names=TRUE)
+names(fn) = ss(list.files("geo_data"), "_")
+datList = lapply(fn, read.delim, row.names=1)
+
+## fix column names
+for(i in seq(along=datList)) colnames(datList[[i]]) = paste0(
+	colnames(datList[[i]]), "_", names(datList)[i])
+
+## get unique genes
+g = sort(unique(unlist(sapply(datList,rownames))))
+sampleNames = do.call("c", sapply(datList, colnames))
+geneCounts = matrix(0, nr = length(g), nc = length(sampleNames),
+	dimnames = list(g, sampleNames))
+
+for(i in seq(along=datList)) {
+	cat(".")
+	geneCounts[rownames(datList[[i]]),colnames(datList[[i]])] = as.matrix(datList[[i]])
+}
+geneCounts = do.call("cbind", datList)
